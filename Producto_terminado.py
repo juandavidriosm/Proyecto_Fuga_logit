65e3ebc3a230e7dbeef5d2063d607a55ca5e9d67
juandavidriosm <122638639+juandavidriosm@users.noreply.github.com>
2024-01-14 23:25:36 +0100

    Add files via upload

---
+++ Producto_terminado.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+df_fuga_sinObj = pd.read_csv("Out/df_fuga_sinObj.csv")
+df_fuga_completo = pd.read_csv("Out/df_fuga_completo.csv")
+
+df_fuga_completo = pd.read_csv("Out/df_fuga_train.csv")
+to_factor = list(df_fuga_completo.loc[:,df_fuga_completo.nunique() <= 4]);  
+df_fuga_completo[to_factor] = df_fuga_completo[to_factor].astype('category')
+
+Y =df_fuga_completo.Fuga
+df_fuga_completo.drop("Fuga",axis = 1,inplace = True)
+
+df_fuga_sinObj = df_fuga_sinObj[df_fuga_completo.columns]
+
+
+pca = PCA(n_components=0.6)
+logreg = LogisticRegression(C=0.001,penalty="l1", solver="liblinear")
+
+pipe_model_interac = Pipeline(steps=[("pca", pca), ("logreg", logreg)])
+
+pipe_model_interac.fit(df_fuga_completo,Y)
+
+
+predictions_finales = pipe_model_interac.predict(df_fuga_sinObj)
+df_fuga_sinObj["Fuga"] = predictions_finales
+
+df_fuga_sinObj.to_csv("Out/PrediccionesFinales.csv")
